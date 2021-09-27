504db282e50700c2277a049e8b6adc0fa401ee01
442009190 <91487767+442009190@users.noreply.github.com>
2021-09-28 02:33:28 +0300

    Update assignment1.java

--- assignment1.java
+++ assignment1.java
@@ -4,7 +4,13 @@
 //442009190
 class Main {
   public static void main(String[] args) {
-      //Start your code 
-      
+   Scanner var = new Scanner(System.in);
+   System.out.println("Enter your name");
+   System name = var.nextline();
+   System.out.println("Enter your age");
+   int age = var.nextInt();
+   if(2021-age < 2000){
+     System.out.println("ita ok");
+  
   }
 }
