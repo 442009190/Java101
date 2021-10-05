405f5b38cb25e797e2b0063bc9f5a62d8fd74cd3
442009190 <91487767+442009190@users.noreply.github.com>
2021-10-05 13:56:40 +0300

    Create Smallest.java

---
+++ Smallest.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class  Main {
+    public static void main(String[] args) { 
+      	Scanner one = new Scanner(System.in);
+           
+        System.out.println("Enter a numbar");
+           int var1 = one.nextInt();
+           int var2 = one.nextInt();
+           int var3 = one.nextInt();
+         int smallest = var1; 
+         
+        if(smallest > var2)
+         smallest = var2;
+        if(smallest > var3)
+         smallest = var3;
+         System.out.println(smallest);
