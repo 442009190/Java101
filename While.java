1a5689228d1fe36596a76bd36c9993a229163aa3
442009190 <91487767+442009190@users.noreply.github.com>
2021-10-21 10:56:49 +0300

    Create While.java

---
+++ While.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	 Scanner rew = new Scanner(System.in);
+	 
+	    int X; 
+		System.out.println("Enter a number in an int number:");
+		X = rew.nextInt();
+		int counter = X;
+		
+		while(counter <= X+10){
+		  System.out.println("The counter is("+X+")");
+		  counter = 1;
+		  
+		    
+		}
+	}
+}
