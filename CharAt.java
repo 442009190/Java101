8405ba812a0049dc6487caefc9cd5cdf399dc452
442009190 <91487767+442009190@users.noreply.github.com>
2021-11-11 11:06:21 +0300

    Create CharAt.java

---
+++ CharAt.java
@@ -0,0 +1,15 @@
+public class Main{
+	public static void main(String[] args) {
+ 
+        String name = "ABCD";
+         
+        int index = name.length() / 2;
+            char letter = name.charAt(index);
+            
+             
+                System.out.print(letter);
+            
+        
+     
+	}
+}
