e144845d0a938d41bef1376572519eb5c315cd85
442009190 <91487767+442009190@users.noreply.github.com>
2021-11-24 18:18:43 +0300

    Update Assignment1.java

--- Assignment1.java
+++ Assignment1.java
@@ -1,21 +1,45 @@
- import java.util.Scanner; 
+   import java.util.Scanner; 
  //Yahia howsaw
  //442009190
 class Main {
   public static void main(String[] args) {
+    /*
+Ask the user to enter two float digits. For example ( 3.5 and 11.25 )
+Print the calculation for the two digits in separate lines:
+Addition +
+Subtraction  -
+Multiplication  *
+Division /
+Modulus %
+Grater >
+Grater or equal
+Less 
+Less or equal
+Not equal
+Equal
+*/
    Scanner scan = new Scanner(System.in); 
 		 
  float  number1;                              
  float  number2;
 	  
-  System.out.println("Enter your  number1: ");
+  System.out.println("Enter your first float  number: ");
    number1 = scan.nextFloat();
-  System.out.println("Enter your number2: ");    
+  System.out.println("Enter your second float number: ");    
    number2 = scan.nextFloat();
-  System.out.println("Don"); 
-   
-    
-       
-  
+ 
+   System.out.println(number1 + number2);
+   System.out.println(number1 - number2);
+   System.out.println(number1 * number2);
+   System.out.println(number1 / number2);
+   System.out.println(number1 % number2);
+   System.out.println(number1 > number2);
+   System.out.println(number1 >= number2);
+   System.out.println(number1 < number2);
+   System.out.println(number1 <= number2);
+   System.out.println(number1 != number2);
+   System.out.println(number1 = number2);
+ 
+        
   }
 }
