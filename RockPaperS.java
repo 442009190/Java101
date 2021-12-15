98bb92cd48e49454fbf116d9f388e0f24b2696bb
442009190 <91487767+442009190@users.noreply.github.com>
2021-12-15 22:28:00 +0300

    Update RockPaperS.java

--- RockPaperS.java
+++ RockPaperS.java
@@ -10,18 +10,14 @@ public class Main {
         do{
             System.out.println("If you want to play enter:\n(r or R) for rock,\n(p or P) for paper,\n(s or S) for scissors\nor Enter any key to exit");
             //get the input from the scanner as string. [ User Input w3schools ]
-             String X ="";
-             if(scan.hasNextLine()){
-              
-              X = scan.nextLine();     
-                 
-             }
+             String X =""; 
+              X = scan.nextLine();  
             //convert the string to lower case by using toLowerCase() [ String w3schools ]
               X = X.toLowerCase();
             //get first char by using charAt() [ String w3schools ]
               char X1 = X.charAt(0);
             //check the first char if it is not r, p, and s then break the loop [ if w3schools ]
-              if( X1!='r'&&X1!='p'&&X1!='s')
+              if( !X1='r'&&!X1='p'&&!X1='s')
               break;
                 //use break keyword [ while w3schools ]
               else{
@@ -35,7 +31,12 @@ public class Main {
               continue;
               }
             //check & match who wins
-              
+              if(X1=(computerChoice)){
+		System.out.println("It's a tie!"); 
+	      }else if(X1='r'&&computerChoice='s')||(X1='s'&&computerChoice='p')||(X1='p'&&computerChoice='r'){
+		      System.out.println("You won")
+	      }else
+		      System.out.println("You lost")
 
             //print new line
             System.out.println("\n\n");
