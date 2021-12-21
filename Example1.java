9f997458dc1af415440d12094181225b2d0f2cd2
442009190 <91487767+442009190@users.noreply.github.com>
2021-12-21 07:12:17 +0300

    Update Example1.java

--- Example1.java
+++ Example1.java
@@ -6,24 +6,24 @@ public class Example {
 		float scan;
 		System.out.print("Enter a Number From 0 to 100 :");
 		scan = myvar.nextFloat();
-		
+		// ( 90 , 100 ) print A
 		if (scan >=90 && scan<=100) {
 		    System.out.println("A"); }
-		
+		// ( 80 , 90 ] print B
 		else if (scan >=80 && scan<=90) {
 		    System.out.println("B"); }
-		
+		//( 70 , 80 ] print c
 		else if (scan >=70 && scan<=79) {
 		    System.out.println("C"); }
-		
+		//( 60 , 70 ] print d
 		else if (scan >=60 && scan<=70) {
 		    System.out.println("D"); }
-		    
+		 // ( 0 , 60 ] print f   
 		else if (scan >=0 && scan<=60) {
 		    System.out.println("F"); }
-        /*
-        ( 90 , 100 ) print A
-        ( 80 , 90 ] print B
-        ( 70 , 80 ] print c
-        ( 60 , 70 ] print d
-        ( 0 , 60 ] print f
+       
+        
+        
+         
+         
+        
