0c4fdc5eaa1e8fab121aee1ba0c9864b343aaeff
442009190 <91487767+442009190@users.noreply.github.com>
2021-12-21 08:22:19 +0300

    Update Small or large.java

--- Small or large.java
+++ Small or large.java
@@ -1,18 +1,18 @@
  import java.util.Scanner;
 public class Small or large
 {
-	public static void main(String[] args) {
-	      Scanner input = new Scanner(System.in);
+	public static void main(String[] args) { 
+	         Scanner input = new Scanner(System.in);
     
     int var;
      System.out.println("Enter a number:"); 
       var = input.nextInt(); 
     if ( var > 0 )
-     System.out.println("var - 1");
+     System.out.println("var= "+(var - 1)+"v");
     else if(var < 0)
-     System.out.println("var + 1");
+     System.out.println("var= "+(var + 1)+"v");
     else 
-    System.out.println("var");
-    
-	}
+    System.out.println("var="+var+"v");
+    }
 }
+	 
